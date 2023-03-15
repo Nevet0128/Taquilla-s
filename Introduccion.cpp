c6945656bc13af65d882cba73e88ca5945ad24f9
Carlos MG <keystrike95@gmail.com>
2023-03-14 21:20:38 -0600

    introducción del proyecto

---
+++ Introduccion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<memory>
+
+using namespace std;
+
+void Introduccion()
+{
+	gtxy(13,4); cout<<"Este programa esta dedicado a llevar un control de personas y de dinero que se necesita para saber"; 
+	gtxy(13,5); cout<<"el numero de dias y de dinero de un determinado dia. El uso de este programa esta orientado"; 
+	gtxy(13,6); cout<<"a lugares con mucha recurrencia, ya sea en un museo, parque de diversiones, circo, etc...";	 
+	gtxy(13,8); cout<<"El programa abarca todo el calendario del anio 2018, tomando en cuenta dias festivos para"; 
+	gtxy(13,9); cout<<"promociones y dias en donde no se trabaja como domingos y dias obligatorios.\n\n\t"<<endl;
+	system("pause");
+	system("cls");
+}
